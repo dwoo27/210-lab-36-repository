26890d0ee1e45f3849a3e65f617224772463a6f6
dwoo27 <dwooed27@gmail.com>
2026-05-06 15:47:30 -0700

    10 min commit

--- 210-lab-36.cpp
+++ 210-lab-36.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "IntBinaryTree.h"
 using namespace std;
 
-int main() {
+const int SIZE = 20000;
 
+int main() {
+    // open the file containing the codes
+    ifstream fin;
+    fin.open("codes.txt");
+    string value;
+   
+    IntBinaryTree tree; //create BST
+
+    if (fin.good()) {
+        cout << "file open";
+        for (int i = 0; i < SIZE; i++) {
+            getline(fin, value);
+            tree.insertNode(value);
+        }
+        fin.close();
+    }
+    else {
+        cout << "File not found";
+    }
+
+    //test tree sucessfully imported
+    tree.displayInOrder();
 
     return 0;
 }
