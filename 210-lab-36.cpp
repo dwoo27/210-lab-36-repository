5c9c75e15c86260c9e50d2bd2b137a9e0945f0ba
dwoo27 <dwooed27@gmail.com>
2026-05-06 16:08:38 -0700

    10 min commit

--- 210-lab-36.cpp
+++ 210-lab-36.cpp
@@ -14,7 +14,7 @@ int main() {
    
     IntBinaryTree tree; //create BST
 
-    if (fin.good()) {
+    if (fin.good()) { //read values from txt and populate bst
         cout << "file open";
         for (int i = 0; i < SIZE; i++) {
             getline(fin, value);
@@ -25,11 +25,38 @@ int main() {
     else {
         cout << "File not found";
     }
-
-    //test tree sucessfully imported
-    tree.displayInOrder();
-
+    //do while for menu
     do {
+        cout << "1. Display records" << endl;
+        cout << "2. Add record" << endl;
+        cout << "3. Delete record" << endl;
+        cout << "4. Search for record" << endl;
+        cout << "5. Modify record" << endl;
+        cout << "6. Quit" << endl;
+        cout << "Enter choice: " << endl;
+       
+        int choice;
+        cin >> choice;
+
+        switch (choice) {
+            //display bst
+            case 1:
+        
+            //add to record
+            case 2:
+            
+            //delete record
+            case 3:
+           
+            //search for record
+            case 4:
+           
+            //modify record
+            case 5:
+
+            //quit
+            case 6:
+        }
 
     } while ();
     return 0;
